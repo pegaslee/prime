c716ac4f4f1c6bf9ed10a50ac84de6c7208c5bcc
Lee Pêgas <lee.pegas@fabfitfun.com>
2021-04-28 23:58:12 +0100

    remove debug print

---
+++ prime_arg.py
@@ -0,0 +1,26 @@
+import math
+import sys
+
+n = int(sys.argv[1])
+
+k = math.isqrt(n)
+
+if k % 2 != 0:
+    a = k
+    b = k
+else:
+    a = k - 1
+    b = k + 1
+
+while a > 1:
+    if a * b == n:
+        break
+    elif a * b < n:
+        b = b + 2
+    elif a * b > n:
+        a = a - 2
+
+if a == 1:
+    print(f"{n} is prime")
+else:
+    print(f"{n} = {a} x {b}")
